9433a2310688ddf9ded0ca6ad8bfd9f8beec286c
unknown <guilhermebueno6@gmail.com>
2019-05-24 16:46:38 -0400

    Added some JS scripting to enable the toggling in order to add grayscale to the svg icons and some css transitions to invert colors upon hovering.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
-(()=>{
-  console.log('Works');
-//Self invoking Function
+(() =>{
+
+console.log('fired!');
+
+
+
+let allImages = document.querySelectorAll(".allImages img");
+
+
+
+function logMyId() {
+console.log(this.id);
+
+
+
+this.classList.toggle("toggledThing")
+
+
+}
+
+
+
+
+
+function logTheSVG(){
+debugger;
+}
+
+
+
+allImages.forEach(item => {
+item.addEventListener("click", logMyId);
+});
+
+
+
+
+
+
 })();
